bb5c143a6d614d8c80cbe13a86b7f4b447360bb5
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-11-27 12:26:07 +0000

    adding user model - password hashing

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 load_dotenv()
 
@@ -9,3 +10,21 @@ app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
 db = SQLAlchemy(app)
+
+
+# user model with password hashing
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120),unique=True, nullable=False)
+    password_hash = db.Column(db.String(360), nullable=False)
+
+    @property
+    def password(self):
+        raise ArithmeticError("Password is not a readable attribute!")
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def __repr__(self) -> str:
+        return f'User email: {self.email} password: {self.password_hash}'
